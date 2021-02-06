7ecf3b8321f7b7c064ab1791cd73d807900c70be
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-02-06 16:12:24 -0500

    Replaced project with pure js implementation

---
+++ src/utils.js
@@ -0,0 +1,38 @@
+const isAndroid = () => {
+    return /Android/i.test(navigator.userAgent);
+  };
+  
+const isiOS = () => {
+    return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+  };
+  
+const isMobile = () => {
+    return isAndroid() || isiOS();
+  };
+  
+const setupCamera = async (video, videoWidth, videoHeight) => {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      throw "Browser API navigator.mediaDevices.getUserMedia not available";
+    }
+  
+    video.width = videoWidth;
+    video.height = videoHeight;
+  
+    const mobile = isMobile();
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        facingMode: "user",
+        width: mobile ? undefined : videoWidth,
+        height: mobile ? undefined : videoHeight
+      }
+    });
+    video.srcObject = stream;
+  
+    return new Promise(resolve => {
+      video.onloadedmetadata = () => {
+        resolve(video);
+      };
+    });
+  };
+  
